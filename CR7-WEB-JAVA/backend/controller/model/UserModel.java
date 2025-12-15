51964aa4c050d114c048d6a4eb627b89729d2d69
aasif-create <aasifmmd12345@gmail.com>
2025-12-15 19:21:09 +0530

    UPDATED

---
+++ CR7-WEB-JAVA/backend/controller/model/UserModel.java
@@ -0,0 +1,17 @@
+package com.cr7.model;
+public class UserModel {
+    private String username;
+    private String password;
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
