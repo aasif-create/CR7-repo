a076ea9ef0a6c7425a688d3d7ebe39aa8c7e0175
aasif-create <aasifmmd12345@gmail.com>
2025-12-15 19:18:58 +0530

    UPDATED

--- CR7-WEB-JAVA/backend/controller/CR7Controller.java
+++ CR7-WEB-JAVA/backend/controller/CR7Controller.java
@@ -1,3 +1,13 @@
+package com.cr7.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CR7Controller {
-    
+
+    @GetMapping("/cr7")
+    public String cr7Info() {
+        return "Cristiano Ronaldo - GOAT of Football";
+    }
 }
